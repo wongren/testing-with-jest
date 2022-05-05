1629dbbcae4c44cd9700d60813243ebd571ea4a0
wongren <81237890+wongren@users.noreply.github.com>
2022-05-05 16:01:33 +0200

    ett eget jest-test

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,17 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+//Skapar ett nytt test(gjorts under labb 5 med christopher, mesam och johans hjälp)
+test('pop on stck with two or more elements that is being removed', () => {
+    //sätter en konstant som vi ska ta bort
+    const actual = "hej"
+    //Lägger till konstanten
+    stack.push(actual)
+    //Kollar så att översta elementet är konstanten
+    expect(stack.peek()).toBe(actual)
+    //Kollar så att konstanten tas bort från stacken
+    expect(stack.pop()).toBe(actual)
+    //Kollar att det som retureneras inte är konstanten
+    expect(stack.peek()).not.toBe(actual)
+});
